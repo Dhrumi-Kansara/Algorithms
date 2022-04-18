a1073990f978a654f8efe9a813f0f65f647b1742
Dhrumi-Kansara <dhrumikansara1999@gmail.com>
2022-04-18 18:40:00 +0530

    continuous maximum sub array sum

--- continuous max subarray sum/continuous_maximum_subarray_sum.c
+++ continuous max subarray sum/continuous_maximum_subarray_sum.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-
-
+int leftBestSumPostion=-1;
+int rightBestSumPostion=-1;
 int calculateCMSS(int a[],int p, int q);
 int calculateCrossSum(int a[],int p, int mid,int q);
 int calculateMax(int ls, int rs, int cs);
@@ -11,8 +11,8 @@ int main() {
   int p=0; /*first element*/
   int q=8; /*last element*/
   int sum = calculateCMSS(a,p,q);
-  printf("\ncontinuous maximum sub array sun: %d", sum);
-  
+  printf("\ncontinuous maximum sub array sun: %d", sum);  
+  printf("\narray index starting from: %d to %d", leftBestSumPostion, rightBestSumPostion);
   return 0;
 }
 
@@ -30,12 +30,15 @@ int calculateCMSS(int a[],int p, int q)
     return a[p];
   } 
   else {
+
     mid=(p+q)/2;
     // printf("\nmid:%d",mid);
+
     ls=calculateCMSS(a,p,mid);/* time complexity T(n/2)*/
     rs=calculateCMSS(a,mid+1,q);/* time complexity T(n/2)*/
     cs=calculateCrossSum(a, p, mid, q);/* time complexity n*/
     max=calculateMax(ls,rs,cs);
+    
     // printf("\n p:%d q:%d ls:%d rs:%d cs:%d max:%d",p,q, ls, rs, cs, max);
 
     return max;
@@ -48,15 +51,21 @@ int calculateCMSS(int a[],int p, int q)
 int calculateCrossSum(int a[],int p, int mid,int q)
 {
   
-  int lts = 0, lbs = 0, lbsp = 0, rts = 0, rbs = 0, rbsp = 0, totalCrossSum=0;
+  int lts = 0; /*left total sum*/
+  int lbs = 0; /*left best sum*/
+  int lbsp = 0; /*left best sum positon*/
+  int rts = 0; /*right total sum*/
+  int rbs = 0; /*right best sum*/
+  int rbsp = 0; /*right best sum positon*/
+  int totalCrossSum=0;
   int i=mid,j=mid+1;
 
   // printf("\n cross sum");
   // printf("\n i:%d j:%d", i, j);
   
-
   /*calculate left total sum*/
   for(i=mid;i>=p;i--) {
+
     lts=lts+a[i];
     // printf("\nlts:%d lbs:%d\n",lts,lbs);
     if(lts>lbs) {
@@ -67,6 +76,7 @@ int calculateCrossSum(int a[],int p, int mid,int q)
 
   /*calculate right total sum*/
     for(j=mid+1;j<=q;j++) {
+
     rts=rts+a[j];
     if(rts>rbs) {
       rbs=rts;
@@ -76,7 +86,8 @@ int calculateCrossSum(int a[],int p, int mid,int q)
   
   // printf("\nlbsp:%d rbsp:%d",lbsp,rbsp);
   // printf("\nlbs:%d rbs:%d",lbs,rbs);
-
+  leftBestSumPostion=lbsp;
+  rightBestSumPostion=rbsp;
   totalCrossSum = lbs+rbs;
   return totalCrossSum;
 }
